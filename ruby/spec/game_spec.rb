a604e6346d2387e36f75d3a2a6356c798c8a9aba
ran.zhao <ran.zhao@ttpod.com>
2012-11-02 00:44:02 +0800

    commit 2x2

--- ruby/spec/game_spec.rb
+++ ruby/spec/game_spec.rb
@@ -8,48 +8,35 @@ end
 describe Game do
   it "1x1 cells" do
     assert_stage [[0]], [[0]]
-
     assert_stage [[1]], [[0]]
   end
 
   it "2x1 cells" do
     assert_stage [[0], [0]], [[0], [0]]
-
     assert_stage [[1], [0]], [[0], [0]]
-
     assert_stage [[1], [1]], [[0], [0]]
   end
 
   it "3x1 cells" do
     assert_stage [[0], [0], [0]], [[0], [0], [0]]
-
     assert_stage [[1], [0], [0]], [[0], [0], [0]]
-
     assert_stage [[1], [1], [0]], [[0], [0], [0]]
-
     assert_stage [[1], [1], [1]], [[0], [1], [0]]
   end
 
   it "4x1 cells" do
     assert_stage [[0], [0], [0], [0]], [[0], [0], [0], [0]]
-
     assert_stage [[1], [0], [0], [0]], [[0], [0], [0], [0]]
-
     assert_stage [[1], [1], [0], [0]], [[0], [0], [0], [0]]
-
     assert_stage [[1], [1], [1], [0]], [[0], [1], [0], [0]]
-
     assert_stage [[0], [1], [1], [1]], [[0], [0], [1], [0]]
-
     assert_stage [[1], [1], [1], [1]], [[0], [1], [1], [0]]
 
   end
 
   it "1x2 cells" do
     assert_stage [[0, 0]], [[0, 0]]
-
     assert_stage [[1, 0]], [[0, 0]]
-
     assert_stage [[1, 1]], [[0, 0]]
   end
 
@@ -58,5 +45,24 @@ describe Game do
     assert_stage [[1, 0, 0]], [[0, 0, 0]]
     assert_stage [[1, 1, 0]], [[0, 0, 0]]
     assert_stage [[1, 1, 1]], [[0, 1, 0]]
+  end
+
+  it "1x4 cells" do
+    assert_stage [[0, 0, 0, 0]], [[0, 0, 0, 0]]
+    assert_stage [[1, 0, 0, 0]], [[0, 0, 0, 0]]
+    assert_stage [[1, 1, 0, 0]], [[0, 0, 0, 0]]
+    assert_stage [[1, 1, 1, 0]], [[0, 1, 0, 0]]
+    assert_stage [[0, 1, 1, 1]], [[0, 0, 1, 0]]
+    assert_stage [[1, 1, 1, 1]], [[0, 1, 1, 0]]
+  end
+
+  it "2x2 cells" do
+    assert_stage [[0, 0], [0, 0]], [[0, 0], [0, 0]]
+    assert_stage [[1, 0], [0, 0]], [[0, 0], [0, 0]]
+    assert_stage [[1, 1], [0, 0]], [[0, 0], [0, 0]]
+    assert_stage [[1, 0], [1, 0]], [[0, 0], [0, 0]]
+    assert_stage [[1, 0], [0, 1]], [[0, 0], [0, 0]]
+    assert_stage [[1, 1], [1, 0]], [[1, 0], [0, 1]]
+    assert_stage [[1, 1], [0, 0]], [[0, 0], [0, 0]]
   end
 end
